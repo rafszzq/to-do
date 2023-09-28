defb599d5eec2be31a909a2a8df761394dee5e49
rafszzq <143035050+rafszzq@users.noreply.github.com>
2023-09-28 17:59:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-.
+function addTask() {
+            const taskInput = document.getElementById("task");
+            const taskText = taskInput.value.trim();
+
+            if (taskText !== "") {
+                const tasksList = document.getElementById("task");
+                const newTask = document.createElement("li");
+                newTask.innerHTML = `
+                    ${taskText}
+                    <button onclick="removeTask(this)">Remover</button>
+                `;
+                tasksList.appendChild(newTask);
+
+                taskInput.value = "";
+            }
+        }
+
+        function removeTask(button) {
+            const taskToRemove = button.parentElement;
+            taskToRemove.remove();
+        }
